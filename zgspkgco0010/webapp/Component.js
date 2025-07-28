57296679727ed404ad8a5406a80b11f8043b2927
dbimgsitm <165974193+dbimgsitm@users.noreply.github.com>
2025-07-28 15:20:34 +0900

    2025.07.28 : 수불부 GL-평가클래스 기준정보

---
+++ zgspkgco0010/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.gsitm.pkg.co.zgspkgco0010.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
