9ae07cab7570cd839c889679b7f0b112c4f95b4a
dbimgsitm <165974193+dbimgsitm@users.noreply.github.com>
2025-07-28 15:21:58 +0900

    2025.07.28 : 원가계산 유형 기준정보

---
+++ zgspkgco0030/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.gsitm.pkg.co.zgspkgco0030.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
